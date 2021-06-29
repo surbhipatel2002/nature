d968be38a3ce47d49dd62f67deb50d9c4014832f
surabhipatel2002 <surabhipatel200@gmail.com>
2021-06-29 15:38:26 +0530

    changes in text

--- js/business.js
+++ js/business.js
@@ -7,6 +7,41 @@ function homepage() {
 function productpage(){
   window.location.href = "product.html";
 }
+function changeImage(data) {
+  console.log(data)
+  if(data=="first"){
+    document.getElementById('myImage').src="images/images(1).jpg";
+  }
+  if(data=="second"){
+    document.getElementById('myImage').src="images/lantern-light-pendant-wall-combo-of-three-matching-lamp-with-original-imaex9msgu6dsshj.jpeg";
+  }
+  if(data=="third"){
+    document.getElementById('myImage').src="images/images.jpg";
+  }
+  if(data=="four"){
+    document.getElementById('myImage').src="images/stripped-shade-wooden-table-lamp-by-the-light-house-stripped-shade-wooden-table-lamp-by-the-light-ho-k1jtnv.jpg";
+  }
+
+}
+
+function review(){
+  //Add
+  document.getElementById("reviews").classList.add("active")
+  document.getElementById("reviews").classList.add("show");
+  document.getElementById("nav-review").classList.add("active");
+
+  
+  //Remove
+  document.getElementById("specifications").classList.remove("show")
+  document.getElementById("specifications").classList.remove("active");
+  document.getElementById("nav-specification").classList.remove("active");
+  
+
+  document.getElementById("about").classList.remove("show");
+  document.getElementById("about").classList.remove("active");
+  document.getElementById("nav-about").classList.remove("active");
+}
+
 function submitFunction() {
   onkeyupFunction();
 }
